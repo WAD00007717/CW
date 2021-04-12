4f1215cb479e7a31391953cb9759e1a3a129e6a6
Khotam <kbakhromov@yandex.ru>
2021-04-13 01:20:42 +0500

    refactor: from dotnet CLI to Visual Studio project

---
+++ SocialMedia/Controllers/PostController.cs
@@ -0,0 +1,96 @@
+﻿using AutoMapper;
+using Domain.DTO;
+using Domain.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Controllers
+{
+    // api/posts
+    [Microsoft.AspNetCore.Mvc.Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IPostRepo _repository;
+
+        public PostsController(IPostRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        // GET api/posts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Post>>> GetAllPosts()
+        {
+            var posts = await  _repository.GetAllPosts();
+            return Ok(_mapper.Map<IEnumerable<PostReadDto>>(posts));
+        }
+
+        // GET api/posts/{id}
+        [HttpGet("{id}", Name = "GetPostById")]
+        public ActionResult<PostReadDto> GetPostById(int id)
+        {
+            var post = _repository.GetPostById(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            return Ok(_mapper.Map<PostReadDto>(post));
+        }
+
+        // POST api/posts
+        [HttpPost]
+        public ActionResult<PostReadDto> CreatePost(PostCreateDto post)
+        {
+            var postModel = _mapper.Map<Post>(post);
+            _repository.CreatePost(postModel);
+            _repository.SaveChanges();
+
+            var postReadDto = _mapper.Map<PostReadDto>(postModel);
+
+            return CreatedAtRoute(nameof(GetPostById), new { Id = postReadDto.Id }, postReadDto);
+            // return Ok(postReadDto);
+        }
+
+        // PUT api/posts/{1}
+        [HttpPut("{id}")]
+        public ActionResult UpdatePost(int id, PostUpdateDto post)
+        {
+            var postModelFromRepo = _repository.GetPostById(id);
+            if (postModelFromRepo == null)
+            {
+                return NotFound();
+            }
+
+            _mapper.Map(post, postModelFromRepo);
+
+            _repository.UpdatePost(postModelFromRepo);
+            _repository.SaveChanges();
+
+            return NoContent();
+        }
+
+        // DELETE api/posts/{1}
+        [HttpDelete("{id}")]
+        public ActionResult DeletePost(int id)
+        {
+            var postModelFromRepo = _repository.GetPostById(id);
+            if (postModelFromRepo == null)
+            {
+                return NotFound();
+            }
+
+            _repository.DeletePost(postModelFromRepo);
+            _repository.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
---
+++ SocialMedia/Startup.cs
@@ -0,0 +1,72 @@
+using AutoMapper;
+using Infrastructure.Data;
+using Infrastructure.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using SocialMedia.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMedia
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer
+               (Configuration.GetConnectionString("SocialMediaConnection")));
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddScoped<IPostRepo, SqlPostRepo>();
+            services.AddSwaggerGen();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
